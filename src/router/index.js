be291cdf93222951cbaceef1dc59e9bef6088d9b
CocoCat56 <zo7758002@gmail.com>
2021-09-16 11:49:29 +0800

    更新 index.js
    更新路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,39 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '*',
+      redirect: '/',
+    },{
+      path: '',
+      component: () => import('@/components/layout'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: () => import('@/components/Home'),
+        },{
+          path: '/search/:searchText',
+          name: 'search',
+          component: () => import('@/components/search'),
+        },{
+          path: '/international/:list1',
+          name: 'international',
+          component: () => import('@/components/international'),
+        },{
+          path: '/nationwide/:list2',
+          name: 'nationwide',
+          component: () => import('@/components/nationwide'),
+        },{
+          path: '/festival/:id',
+          name: 'festival',
+          component: () => import('@/components/festival'),
+        }
+      ]
     }
   ]
-})
+});
