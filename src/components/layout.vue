c1a593ff39e5b042e902dbe63a358ae2cba0b3b7
CocoCat56 <zo7758002@gmail.com>
2021-09-16 11:34:28 +0800

    建立 layout.vue 和 navbar.vue
    建立基本版面 (navbar, footer)

---
+++ src/components/layout.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="layout">
+    <navbar></navbar>
+    <router-view></router-view>
+    <footer>
+      <div class="container-md">
+        <p>本站僅作品練習，圖資來源為文化部文化資料開放服務網，無任何商業營利用途</p>
+        <p>使用之資料集為<a href="https://opendata.culture.tw/frontsite/openData/detail?datasetId=413" target="_blank">文化部[2015-2021][藝文活動-節慶專區]</a></p>
+        <p>此開放資料依政府資料開放授權條款 (Open Government Data License) 進行公眾釋出，使用者於遵守本條款各項規定之前提下，得利用之。</p>
+        <p>政府資料開放授權條款：<a href="https://data.gov.tw/license" target="_blank">https://data.gov.tw/license</a></p>
+        <p>© 2021 All Right Reserved | Design by 
+          <a href="https://coco-personal-portfolio.netlify.app/" target="_blank">劉盈秀</a></p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+import navbar from '@/components/navbar';
+
+export default {
+  name: 'layout',
+  components: {
+    navbar,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  $color-light-grey: #F5F5F5;
+  $color-orange: #ff6d00;
+  $color-black: #535353;
+  footer {
+    padding: 2rem 0;
+    background-color: $color-light-grey;
+    font-size: 14px;
+    color: $color-black; 
+    a {
+      color: $color-orange;
+      text-decoration: none;
+      &:hover {
+        font-weight: bolder;
+      }
+    }
+  }
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,80 @@
+<template>
+  <nav class="navbar navbar-expand-sm navbar-light fixed-top
+  " style="background-color: #fff;">
+    <div class="container-md">
+      <router-link class="mr-4" to="/">FESTIVAL</router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/international/list1">國際級</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/nationwide/list2">全國級</router-link>
+          </li>
+        </ul>
+        <input class="mr-sm-2 mt-3 mt-sm-0" type="search" placeholder="請輸入節慶名稱" aria-label="Search" v-model="searchText" @keyup.enter="search(searchText)">
+        <a @click.prevent="search(searchText)"><i class="fas fa-search"></i></a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  data () {
+    return {
+      searchText: '',
+    }
+  },
+  methods: {
+    search(text) {
+      this.$router.push(`/search/${text}`);
+      this.searchText = '';
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Karantina&display=swap');
+  $color-grey: #C4C4C4;
+  $color-light-grey: #F5F5F5;
+  $color-orange: #ff6d00;
+  nav {
+    box-shadow: 2px 2px 1px $color-light-grey;
+  }
+  .navbar-light .container-md>a {
+    font-size: 2.3rem;
+    font-family: 'Karantina', cursive;
+    color: $color-orange;
+    text-decoration: none;
+  }
+  .nav-link {
+    cursor: pointer;
+    &:hover {
+      color: $color-orange !important;
+    }
+  }
+  input {
+    border-radius: 10px;
+    outline: none;
+    border: none;
+    padding-left: 10px;
+    background-color: $color-light-grey;
+    font-size: 14px;
+    line-height: 2;
+  }
+  .fa-search {
+    color: $color-grey;
+    cursor: pointer;
+    &:hover {
+      color: $color-orange;
+    }
+  }
+</style>
