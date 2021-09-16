3142985aa929135a3d4a36f7cb7d79fbd24dca5f
CocoCat56 <zo7758002@gmail.com>
2021-09-16 11:42:38 +0800

    建立 search.vue
    建立搜尋頁面

---
+++ src/components/search.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container-md">
+    <h4>Search：{{searchText}}</h4>
+    <festivals></festivals>
+  </div>
+</template>
+
+<script>
+import festivals from './festivals.vue';
+export default {
+  name: 'search',
+  data () {
+    return {
+      searchText: ''
+    }
+  },
+  components: {
+    festivals
+  },
+  created() {
+    const vm = this;
+    if(vm.$route.params.searchText) {
+        vm.searchText = vm.$route.params.searchText;
+        console.log(vm.searchText);
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .container-md {
+    padding-top: 100px;
+  }
+  h4 {
+      margin-left: 2rem;
+  }
+</style>
