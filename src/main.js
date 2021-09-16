017fcd2f94c5e7e67196b08b5eab331abb2f3bb4
CocoCat56 <zo7758002@gmail.com>
2021-09-16 11:22:22 +0800

    update main.js
    引入 bootstrap, fontawesome

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import axios from 'axios'; 
-import VueAxios from 'vue-axios'; 
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 import App from './App';
 import router from './router';
+import 'bootstrap';  // bootstrap js file
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
