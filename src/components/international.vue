4fbdfaf0cae187168cb582bbe0ec083fc52a0fa8
CocoCat56 <zo7758002@gmail.com>
2021-09-16 11:45:42 +0800

    建立 international 和 nationwide
    建立國際級和全國級頁面

---
+++ src/components/international.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container-md">
+    <h4>國際級</h4>
+    <festivals></festivals>
+  </div>
+</template>
+
+<script>
+import festivals from './festivals.vue';
+export default {
+  name: 'international',
+  data () {
+    return {}
+  },
+  components: {
+    festivals
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .container-md {
+    padding-top: 100px;
+  }
+  h4 {
+    margin-left: 2rem;
+  }
+</style>
---
+++ src/components/nationwide.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container-md">
+    <h4>全國級</h4>
+    <festivals></festivals>
+  </div>
+</template>
+
+<script>
+import festivals from './festivals.vue';
+export default {
+  name: 'nationwide',
+  data () {
+    return {}
+  },
+  components: {
+    festivals
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .container-md {
+    padding-top: 100px;
+  }
+  h4 {
+    margin-left: 2rem;
+  }
+</style>
